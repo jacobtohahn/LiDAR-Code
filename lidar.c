5c8d90fa0914a6dd6c2e7f2adc975303888e3e7e
Jacob Hahn <jtohahn@gmail.com>
2024-01-09 12:38:33 -0500

    Reading again

--- lidar.c
+++ lidar.c
@@ -16,43 +16,8 @@ int serial_port;
 void initializeLidar() {
     // Open the serial port in read/write mode
     serial_port = open("/dev/ttyS0", O_RDWR);
-
-    // Create new termios struct, we call it 'tty' for convention
-    struct termios tty;
-
-    // Read in existing settings, and handle any error
-    if(serial_port < 0) {
+    if (serial_port < 0) {
         printf("Error %i from open: %s\n", errno, strerror(errno));
-        exit(1);
-    }
-
-    tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity
-    tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication
-    tty.c_cflag |= CS8; // 8 bits per byte
-    tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control
-    tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines
-
-    tty.c_lflag &= ~ICANON;
-    tty.c_lflag &= ~ECHO; // Disable echo
-    tty.c_lflag &= ~ECHOE; // Disable erasure
-    tty.c_lflag &= ~ECHONL; // Disable new-line echo
-    tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
-    tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
-    tty.c_iflag &= ~(IGNBRK | BRKINT | PARMRK | ISTRIP | INLCR | IGNCR | ICRNL); // Disable any special handling of received bytes
-
-    tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes
-    tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
-
-    tty.c_cc[VTIME] = 10; // Wait for up to 1s, returning as soon as any data is received.
-    tty.c_cc[VMIN] = 0;
-
-    // Set in/out baud rate to be 230400
-    cfsetispeed(&tty, B230400);
-    cfsetospeed(&tty, B230400);
-
-    // Save tty settings, also checking for error
-    if (tcsetattr(serial_port, TCSANOW, &tty) != 0) {
-        printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
         exit(1);
     }
 }
@@ -63,9 +28,10 @@ void readLidarDataPacket(unsigned char *dataPacket) {
     // For example, read from the serial port into dataPacket
     unsigned char read_byte;
     read(serial_port, &read_byte, 1);
-    if (read_byte == START_CHARACTER) { 
-        printf(read_byte);
-    }
+    printf("%x\n", read_byte);
+    // if (read_byte == START_CHARACTER) { 
+    //     printf(read_byte);
+    // }
 }
 
 // Function to process the LiDAR data and convert it into a top-down view
